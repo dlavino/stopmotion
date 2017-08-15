6f8ade53523d9c57f3da33b69eb91dd997535e4e
dlavino <daniel_lavino@hotmail.com>
2017-08-14 23:09:47 -0300

    teste

--- stopmotion.py
+++ stopmotion.py
@@ -181,7 +181,7 @@ while key!= ord('q'):
                             actIcon -= 1
                         if (actIcon == 0) and (actSeqIcon > 0):
                             for i in range(0,9):
-                                black[screen_height*0.9:screen_height*0.9 + icon_height, (8-i)*icon_width:(8-i)*icon_width + icon_width] = seqIcon[actSeqIcon-i]
+                                black[screen_height*0.9:screen_height*0.9 + icon_height, (8-i)*icon_width:(8-i)*icon_width + icon_width] = seqIcon[actSeqIcon-1-i]
                                 cv2.rectangle(black,(i*icon_width,int(screen_height*0.9)),(icon_width + i*icon_width,screen_height),(255,255,255),3)
                             cv2.rectangle(black,(8*icon_width,int(screen_height*0.9)),(icon_width + 8*icon_width,screen_height),(0,255,0),3)
                     cv2.rectangle(black,(actIcon*icon_width,int(screen_height*0.9)),(icon_width + actIcon*icon_width,screen_height),(0,0,0),-1)
