81a22c16bf6301432b83fc033d289ee78bedcdd5
dlavino <daniel_lavino@hotmail.com>
2017-08-14 22:10:41 -0300

    teste

--- stopmotion.py
+++ stopmotion.py
@@ -182,7 +182,7 @@ while key!= ord('q'):
                                 cv2.rectangle(black,(i*icon_width,int(screen_height*0.9)),(icon_width + i*icon_width,screen_height),(255,255,255),3)
                         else:
                             actIcon -= 1
-                        if actIcon == 0:
+                        if (actIcon == 0) and (actSeqIcon > 0):
                             cv2.rectangle(black,(8*icon_width,int(screen_height*0.9)),(icon_width + 8*icon_width,screen_height),(0,255,0),3)
                     cv2.rectangle(black,(actIcon*icon_width,int(screen_height*0.9)),(icon_width + actIcon*icon_width,screen_height),(0,0,0),-1)
                     cv2.rectangle(black,(actIcon*icon_width,int(screen_height*0.9)),(icon_width + actIcon*icon_width,screen_height),(255,255,255),3)
