e5990ca94f95b0ebc3b3d2d119f2f4801a059a7a
dlavino <daniel_lavino@hotmail.com>
2017-08-14 23:06:01 -0300

    teste

--- stopmotion.py
+++ stopmotion.py
@@ -154,7 +154,6 @@ while key!= ord('q'):
     if (key == ord('c')) or GPIO.input(6):
         for i in range(0,9):
             cv2.rectangle(black,(i*icon_width,int(screen_height*0.9)),(icon_width + i*icon_width,screen_height),(255,255,255),3)
-            print i
         cap()
         black[screen_height*0.9:screen_height*0.9 + icon_height, actIcon*icon_width:actIcon*icon_width + icon_width] = seqIcon[actSeqIcon-1]
         cv2.rectangle(black,(actIcon*icon_width,int(screen_height*0.9)),(icon_width + actIcon*icon_width,screen_height),(0,255,0),3)
@@ -181,7 +180,7 @@ while key!= ord('q'):
                         else:
                             actIcon -= 1
                         if (actIcon == 0) and (actSeqIcon > 0):
-                            for i in range(0,8):
+                            for i in range(0,9):
                                 black[screen_height*0.9:screen_height*0.9 + icon_height, (8-i)*icon_width:(8-i)*icon_width + icon_width] = seqIcon[actSeqIcon-i]
                                 cv2.rectangle(black,(i*icon_width,int(screen_height*0.9)),(icon_width + i*icon_width,screen_height),(255,255,255),3)
                             cv2.rectangle(black,(8*icon_width,int(screen_height*0.9)),(icon_width + 8*icon_width,screen_height),(0,255,0),3)
