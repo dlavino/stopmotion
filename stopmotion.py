acc56a828ad1a0518313540b129181a21c5d36df
dlavino <daniel_lavino@hotmail.com>
2017-08-14 22:17:55 -0300

    teste

--- stopmotion.py
+++ stopmotion.py
@@ -177,12 +177,12 @@ while key!= ord('q'):
                         actSeqIcon -= 1
                         if actIcon == 0:
                             actIcon = 8
+                            for i in range(0,9):
+                                black[screen_height*0.9:screen_height*0.9 + icon_height, (actIcon-i)*icon_width:(actIcon-i)*icon_width + icon_width] = seqIcon[actSeqIcon-i]
+                                cv2.rectangle(black,(i*icon_width,int(screen_height*0.9)),(icon_width + i*icon_width,screen_height),(255,255,255),3)
                         else:
                             actIcon -= 1
                         if (actIcon == 0) and (actSeqIcon > 0):
-                            for i in range(0,8):
-                                black[screen_height*0.9:screen_height*0.9 + icon_height, (actIcon-i)*icon_width:(actIcon-i)*icon_width + icon_width] = seqIcon[actSeqIcon-i]
-                                cv2.rectangle(black,(i*icon_width,int(screen_height*0.9)),(icon_width + i*icon_width,screen_height),(255,255,255),3)
                             cv2.rectangle(black,(8*icon_width,int(screen_height*0.9)),(icon_width + 8*icon_width,screen_height),(0,255,0),3)
                     cv2.rectangle(black,(actIcon*icon_width,int(screen_height*0.9)),(icon_width + actIcon*icon_width,screen_height),(0,0,0),-1)
                     cv2.rectangle(black,(actIcon*icon_width,int(screen_height*0.9)),(icon_width + actIcon*icon_width,screen_height),(255,255,255),3)
